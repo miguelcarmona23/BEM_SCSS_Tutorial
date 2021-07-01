a3701ae84ba7a73cfa2699edabab9c5f2bc32de3
Miguel Carmona <miguelcarmona996@gmail.com>
2021-07-01 11:26:30 -0400

    Added js form

--- JS/script.js
+++ JS/script.js
@@ -9,3 +9,14 @@ function userclick() {
     }
     document.getElementById("demo").innerHTML = text;
 }
+
+function myFunction() {
+    let x = document.getElementById("numb").value;
+    let text;
+    if (isNaN(x) || x < 1 || x > 10) {
+        text = "THAT'S NOT A NUMBER!!!"
+    } else {
+        text = "That most certainly is a number"
+    }
+    document.getElementById("answer").innerHTML = text;
+}
